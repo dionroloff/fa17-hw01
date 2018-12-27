4d96fdb626449d54b27f559c2a9e8078521a2e83
Dion Roloff <dion.roloff@gmail.com>
2018-12-27 13:25:26 -0600

    ex2 completed

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,8 +4,16 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Carry out calculations to find the nth Fibonacci number */
+	    if (position <= 1) {
+			return 1;
+		}
+
+		return calculate(position- 1) + calculate(position - 2);
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() payable {
+		...
+	}
 
 }
